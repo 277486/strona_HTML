9f5484eed56b43cb5d3e012aaa4c94af53fc6a16
Emilia Porczyńska <272013@student.pwr.edu.pl>
2024-07-04 14:56:54 +0200

    poprawa

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, flash, render_template, request
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
+app.secret_key = "supersecretkey"
 
-ex_dict = {'zad1': [-1], 'zad2': ['5 - 5i', '5-5i', '-5i + 5', '-5i+5'],
-           'zad3': ['-4 - 6i', '-4-6i', '-6i - 4', '-6i-4'],
-           'zad4': ['2i'], 'zad5': [4], 'zad6': [13], 'zad7': [6], 'zad8': [4],
-           'zad9': ['8 - 8i', '8-8i', '-8i + 8', '-8i+8'],
-           'zad10': ['-1/8i', '-1/8 i'], 'zad11': [1]}
+ex_dict = {
+    "zad1": ["-1"],
+    "zad2": ["5 - 5i"],
+    "zad3": ["-4 - 6i"],
+    "zad4": ["2i"],
+    "zad5": ["4"],
+    "zad6": ["13"],
+    "zad7": ["6"],
+    "zad8": ["4"],
+    "zad9": ["8 - 8i"],
+    "zad10": ["-1/8i"],
+    "zad11": ["1"],
+}
 
 
 @app.route("/")
@@ -35,9 +43,10 @@ def exercises():
     user_answer = {}
     if request.method == "POST":
         for i in range(1, 12):
-            name = f'zad{i}'
-            user_answer[name] = request.form[name]
+            name = f"zad{i}"
+            user_answer[name] = request.form.get(name)
             correct_answer = ex_dict[name]
+            print(user_answer)
             if user_answer[name] in correct_answer:
                 flash("Dobra odpowieź!", "success")
             if user_answer[name] and user_answer[name] not in correct_answer:
